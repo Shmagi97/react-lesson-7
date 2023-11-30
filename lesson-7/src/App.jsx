831f61bc3d39dbf97f5ef9e6f2fc477bfc227cf3
Shmagi97 <135329382+Shmagi97@users.noreply.github.com>
2023-12-01 00:21:14 +0400

    no end
    no end

---
+++ lesson-7/src/App.jsx
@@ -0,0 +1,22 @@
+
+// import './App.css'
+
+import BasicExample from "./sacdeli"
+
+
+function App() {
+   
+  
+
+  return (
+
+    
+   <BasicExample/>
+
+    
+    
+    
+  )
+}
+
+export default App
